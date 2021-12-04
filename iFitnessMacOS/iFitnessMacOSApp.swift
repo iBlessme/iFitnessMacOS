5339da58c490dfb4dbbe779608b81754e5027007
iblessme <vldikk12@gmail.com>
2021-12-04 12:14:13 +0300

    Initial Commit

---
+++ iFitnessMacOS/iFitnessMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFitnessMacOSApp.swift
+//  iFitnessMacOS
+//
+//  Created by iBlessme on 04.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct iFitnessMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
